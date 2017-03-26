975f60432557e67b67ebc207355dc91eebb40a23
florinolariuip <olariu@gmail.com>
2017-03-26 09:33:51 +0300

    Add comments

--- src/DemoAfter/MessageService.java
+++ src/DemoAfter/MessageService.java
@@ -2,6 +2,11 @@ package DemoAfter;
 
 public class MessageService {
 
+	/**
+	 * Salutation method.
+	 * @param title
+	 * @return
+	 */
 	public String salutation(Title title) {
 		SalutationContext context = new SalutationContext();
 		return context.doExecute(title);
